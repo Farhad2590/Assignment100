5290b5875d2ceb44f685b2ff198e8ae885ff9112
Farhad2590 <76680813+Farhad2590@users.noreply.github.com>
2021-05-30 11:22:55 +0600

    Add files via upload

---
+++ JavaAppleat/src/JAppletDemo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.swing.JApplet;
+import java.awt.*;
+
+/**
+ *
+ * @author farha
+ */
+public class JAppletDemo extends JApplet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    @Override
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+    // TODO overwrite start(), stop() and destroy() methods
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawString("Port City International University", 20, 40);
+         
+    }
+}
